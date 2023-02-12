6a90dbfd77a3befc95df2d4fb46a0d5c214b7fcd
M0VUB <76499782+ShaYmez@users.noreply.github.com>
2023-02-12 23:01:13 +0000

    Update app_rpt.c
    Fix crash when large number of nodes are connected as outlined by DU8BL, upstream fix, develop branch #42

--- asterisk/apps/app_rpt.c
+++ asterisk/apps/app_rpt.c
@@ -7201,8 +7201,7 @@ static int rpt_do_lstats(int fd, int argc, char *argv[])
 
 static int rpt_do_xnode(int fd, int argc, char *argv[])
 {
-	int i,j;
-	char ns;
+	int i,j,ns;
 	char lbuf[MAXLINKLIST],*strs[MAXLINKLIST];
 	struct rpt *myrpt;
 	struct ast_var_t *newvariable;
@@ -7441,8 +7440,7 @@ static int rpt_do_xnode(int fd, int argc, char *argv[])
 
 static int rpt_do_nodes(int fd, int argc, char *argv[])
 {
-	int i,j;
-	char ns;
+	int i,j,ns;
 	char lbuf[MAXLINKLIST],*strs[MAXLINKLIST];
 	struct rpt *myrpt;
 	if(argc != 3)
